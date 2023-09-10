f94700be140d4079ff883a900075022f8bcbeb01
AliceTxT <64071183+AliceTxT@users.noreply.github.com>
2023-09-10 13:38:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,194 @@
+//body_About 450px
+//body_Commissions 230px
+//body_Character 2000px
+//body_Illustration 1000px
+//body_Twitch 1000px
+//body_TC 2450px
+//body_Order 360px
+
+//document.getElementById().style.height = "";
+//document.getElementById().style.opacity = "";
+
+    //document.getElementById("body_About").style.height = "0px";
+    //document.getElementById("body_Commissions").style.height = "0px";
+    //document.getElementById("body_Character").style.height = "0px";
+    //document.getElementById("body_Illustration").style.height = "0px";
+    //document.getElementById("body_Twitch").style.height = "0px";
+    //document.getElementById("body_TC").style.height = "0px";
+    //document.getElementById("body_Order").style.height = "0px";
+
+    //document.getElementById("body_About").style.opacity = "0";
+    //document.getElementById("body_Commissions").style.opacity = "0";
+    //document.getElementById("body_Character").style.opacity = "0";
+    //document.getElementById("body_Illustration").style.opacity = "0";
+    //document.getElementById("body_Twitch").style.opacity = "0";
+    //document.getElementById("body_TC").style.opacity = "0";
+    //document.getElementById("body_Order").style.opacity = "0";
+
+
+function showHome() {
+    //document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    //document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_About").style.height = "450px";
+    document.getElementById("body_About").style.opacity = "1";
+}
+
+function showComms() {
+    document.getElementById("body_About").style.height = "0px";
+    //document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    //document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_Commissions").style.height = "230px";
+    document.getElementById("body_Commissions").style.opacity = "1";
+}
+
+function showChar() {
+    document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    //document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    //document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_Character").style.height = "1000px";
+    document.getElementById("body_Character").style.opacity = "1";
+}
+
+function showIllus() {
+    document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    //document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    //document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_Illustration").style.height = "1000px";
+    document.getElementById("body_Illustration").style.opacity = "1";
+}
+
+
+function showTwitch() {
+    document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    //document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    //document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_Twitch").style.height = "1000px";
+    document.getElementById("body_Twitch").style.opacity = "1";
+}
+
+
+function showTC() {
+    document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    //document.getElementById("body_TC").style.height = "0px";
+    document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    //document.getElementById("body_TC").style.opacity = "0";
+    document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_TC").style.height = "2500px";
+    document.getElementById("body_TC").style.opacity = "1";
+}
+
+function showOrder() {
+    document.getElementById("body_About").style.height = "0px";
+    document.getElementById("body_Commissions").style.height = "0px";
+    document.getElementById("body_Character").style.height = "0px";
+    document.getElementById("body_Illustration").style.height = "0px";
+    document.getElementById("body_Twitch").style.height = "0px";
+    document.getElementById("body_TC").style.height = "0px";
+    //document.getElementById("body_Order").style.height = "0px";
+
+    document.getElementById("body_About").style.opacity = "0";
+    document.getElementById("body_Commissions").style.opacity = "0";
+    document.getElementById("body_Character").style.opacity = "0";
+    document.getElementById("body_Illustration").style.opacity = "0";
+    document.getElementById("body_Twitch").style.opacity = "0";
+    document.getElementById("body_TC").style.opacity = "0";
+    //document.getElementById("body_Order").style.opacity = "0";
+
+    document.getElementById("body_Order").style.height = "360px";
+    document.getElementById("body_Order").style.opacity = "1";
+}
+
+
+//document.getElementById("body_About").style.height = "450px";
+//document.getElementById("body_Commissions").style.height = "230px";
+//document.getElementById("body_Character").style.height = "2000px";
+//document.getElementById("body_Illustration").style.height = "1000px";
+
+
+//document.getElementById("body_About").style.height = "0px";
+//document.getElementById("body_Commissions").style.height = "0px";
+//document.getElementById("body_Character").style.height = "0px";
+//document.getElementById("body_Illustration").style.height = "0px";
+//document.getElementById("body_Illustration").style.height = "0px";
+
+
+function test() {
+    console.log("Hello World!");
+}
